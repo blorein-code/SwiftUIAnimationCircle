5f2a8d56ea58bcbb7fd18583421339f80d6014c7
Berke <berke-topcu@hotmail.com>
2022-11-26 21:48:32 +0300

    Circle animation done

---
+++ SwiftUIShapeProject/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  SwiftUIShapeProject
+//
+//  Created by Berke Topcu on 25.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var firstCirclePercentage : Double = 0
+    @State var secondCirclePercentage : Double = 0
+    @State var thirdCirclePercentage : Double = 0
+    
+    var body: some View {
+     
+        ZStack {
+            Circular(lineWidth: 40, backgroundColor: Color.blue.opacity(0.2), foregroundColor: Color.blue, percentage: firstCirclePercentage)
+                .frame(width: 350,height: 350)
+                .onTapGesture {
+                    if self.firstCirclePercentage == 0 {
+                        self.firstCirclePercentage = 50
+                    } else {
+                        self.firstCirclePercentage = 0
+                    }
+                }
+            Circular(lineWidth: 40, backgroundColor: Color.red.opacity(0.2), foregroundColor: Color.red, percentage: secondCirclePercentage)
+                .onTapGesture {
+                    if self.secondCirclePercentage == 0 {
+                        self.secondCirclePercentage = 35
+                    } else {
+                        self.secondCirclePercentage = 0
+                    }
+                }
+                .padding(80)
+            
+            
+            Circular(lineWidth: 40, backgroundColor: Color.green.opacity(0.2), foregroundColor: Color.green, percentage: thirdCirclePercentage)
+                .onTapGesture {
+                    if self.thirdCirclePercentage == 0 {
+                        self.thirdCirclePercentage = 35
+                    } else {
+                        self.thirdCirclePercentage = 0
+                    }
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIShapeProject/SwiftUIShapeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShapeProjectApp.swift
+//  SwiftUIShapeProject
+//
+//  Created by Berke Topcu on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShapeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
